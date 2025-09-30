4344a313f32e79dcc7f2d639b9861c84ed1f1dbd
Gabriel-affont <162576104+Gabriel-affont@users.noreply.github.com>
2025-10-01 01:02:36 +0300

    Added ingredients

--- main.js
+++ main.js
@@ -4,16 +4,27 @@ async function getRandomMeal() {
     const data = await res.json();
     const meal = data.meals[0];
 
-    
     const mealContainer = document.getElementById("mealContainer");
 
-    
+    let ingridientsList = "";
+    for(let i = 1; i <= 20; i++) {
+        const ingridient = meal[`strIngredient${i}`]; 
+        const measure = meal[`strMeasure${i}`]; 
+        
+       
+        if(ingridient && ingridient.trim() !== "") {
+            ingridientsList += `<li>${ingridient} - ${measure}</li>`;
+        }
+    }
+
     mealContainer.innerHTML = `
       <h2>${meal.strMeal}</h2>
       <img src="${meal.strMealThumb}" alt="${meal.strMeal}" width="300" />
       <p><strong>Category:</strong> ${meal.strCategory}</p>
-        <p><strong>Area:</strong> ${meal.strArea}</p>
-        <p><strong>Instructions:</strong> ${meal.strInstructions}</p>
+      <p><strong>Area:</strong> ${meal.strArea}</p>
+      <p><strong>Instructions:</strong> ${meal.strInstructions}</p>
+      <h3>Ingredients:</h3>
+      <ul>${ingridientsList}</ul>
     `;
 
   } catch (error) {
